5914562e234b999fd16e265e101256f1ce494a7e
GarvGupta-2005 <garv.0603@gmail.com>
2025-07-10 23:22:43 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,12 @@ cloudinaryConnect()
 
 //middlewares 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'token'],
+  credentials: true
+}));
 
 
 //Api end points
